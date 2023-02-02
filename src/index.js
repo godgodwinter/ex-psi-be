8341e134ea5baca0fcdc4ff9d48865fa892d839c
godgodwinter <kakadz.pro@gmail.com>
2023-02-02 16:22:53 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,37 +1,64 @@
-// const express = require("express");
+// // const express = require("express");
+// import express from "express";
+// const app = express();
+
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import dotenv from "dotenv";
+
+// // settings Library
+// dotenv.config();
+// app.use(express.json());
+// app.use(cors());
+// express.urlencoded({ extended: true });
+
+
+// // REST
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
+// app.get('/expressjs', function (req, res) {
+//     res.send('Hello World, ini expressJS');
+// });
+
+// // import PostRoute from "./app/routes/PostRoute.js";
+// // app.use(PostRoute);
+
+// // // SETTINGS SERVER
+// // const PORT = 8001;
+// // // app.listen(PORT, () => {
+// // //     console.log(`Server is running on http://localhost:${PORT}`);
+// // // });
+
+// // app.listen();
+
+// // Listen
+// var port = process.env.PORT || 3000;
+// app.listen(port);
+// console.log('Listening on localhost:' + port);
+
+
+
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var message = 'It works!\n',
+//         version = 'NodeJS ' + process.versions.node + '\n',
+//         response = [message, version].join('\n');
+//     res.end(response);
+// });
+// server.listen();
+
 import express from "express";
 const app = express();
-
-import bodyParser from "body-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-
-// settings Library
-dotenv.config();
-app.use(express.json());
-app.use(cors());
-express.urlencoded({ extended: true });
-
-
-// REST
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// var app = express();
+// Routes
+app.get('/', function (req, res) {
+    res.send('Hello World, ini Root');
 });
 app.get('/expressjs', function (req, res) {
     res.send('Hello World, ini expressJS');
 });
-
-// import PostRoute from "./app/routes/PostRoute.js";
-// app.use(PostRoute);
-
-// // SETTINGS SERVER
-// const PORT = 8001;
-// // app.listen(PORT, () => {
-// //     console.log(`Server is running on http://localhost:${PORT}`);
-// // });
-
-// app.listen();
-
 // Listen
 var port = process.env.PORT || 3000;
 app.listen(port);
